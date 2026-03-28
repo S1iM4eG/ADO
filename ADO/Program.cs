f33101148002a7d37afe89ea50d011332fc00f7d
S1iM <S1iM@HOME-PC>
2026-03-28 22:31:39 +0400

    Add project files.

---
+++ ADO/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Program
+    {
+        static SqlConnection connection;
+        static void Main(string[] args)
+        {
+            string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MoviesPV_522;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            Console.WriteLine(connection_string);
+            connection = new SqlConnection(connection_string);
+
+            string cmd = "SELECT * FROM Directors";
+            Select(cmd);
+            Console.WriteLine($"Количество записей: {Scalar("SELECT COUNT(*) FROM Directors")}");
+            Select("SELECT * FROM Movies");
+        }
+
+        static void Select(string cmd)
+        {
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            for (int i = 0; i < reader.FieldCount; i++)
+                Console.Write($"{reader.GetName(i)}\t");
+            Console.WriteLine();
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+
+            }
+            reader.Close();
+            connection.Close();
+        }
+        static object Scalar(string cmd)
+        {
+            object value = null;
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            value = command.ExecuteScalar();
+            connection.Close();
+            return value;
+        }
+    }
+}
---
+++ ADO/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ADO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ADO")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68f627f3-368e-499e-9c65-d9051d2835cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
