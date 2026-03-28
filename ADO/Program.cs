826fb62705f653a75d2b852dde4c988b0371cfad
S1iM <S1iM@HOME-PC>
2026-03-28 23:09:53 +0400

    Class Select Create

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using ADO.Select;
 
 namespace ADO
 {
@@ -17,28 +18,28 @@ namespace ADO
             connection = new SqlConnection(connection_string);
 
             string cmd = "SELECT * FROM Directors";
-            Select(cmd);
+            Select select = new;
             Console.WriteLine($"Количество записей: {Scalar("SELECT COUNT(*) FROM Directors")}");
             Select("SELECT * FROM Movies");
         }
 
-        static void Select(string cmd)
-        {
-            SqlCommand command = new SqlCommand(cmd, connection);
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            for (int i = 0; i < reader.FieldCount; i++)
-                Console.Write($"{reader.GetName(i)}\t");
-            Console.WriteLine();
-            while (reader.Read())
-            {
-                Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+        //static void Select(string cmd)
+        //{
+        //    SqlCommand command = new SqlCommand(cmd, connection);
+        //    connection.Open();
+        //    SqlDataReader reader = command.ExecuteReader();
+        //    for (int i = 0; i < reader.FieldCount; i++)
+        //        Console.Write($"{reader.GetName(i)}\t");
+        //    Console.WriteLine();
+        //    while (reader.Read())
+        //    {
+        //        Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
 
 
-            }
-            reader.Close();
-            connection.Close();
-        }
+        //    }
+        //    reader.Close();
+        //    connection.Close();
+        //}
         static object Scalar(string cmd)
         {
             object value = null;
