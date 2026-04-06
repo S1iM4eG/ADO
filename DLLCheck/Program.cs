21a9756a292ba976f44da6e7f4e4134613fb1514
S1iM <S1iM@HOME-PC>
2026-04-06 20:54:31 +0400

    DBtools DLL DONE

---
+++ DLLCheck/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using DBtools;
+
+namespace DLLCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connector connector = new Connector(ConfigurationManager.ConnectionStrings["MoviesPV_522"].ConnectionString);
+            connector.Select("*", "Directors");
+            connector.Select("title,release_date,first_name,last_name", "Movies,Directors", "director = director_id");
+            Connector connectorAcademy = new Connector(ConfigurationManager.ConnectionStrings["PV_522_Import"].ConnectionString);
+            connectorAcademy.Select("*", "Disciplines");
+        }
+    }
+}
