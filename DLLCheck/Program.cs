e6de3a8abc72c533bf41daac3b1ef938a5f82756
S1iM <S1iM@HOME-PC>
2026-04-07 21:19:35 +0400

    Update DONE

--- DLLCheck/Program.cs
+++ DLLCheck/Program.cs
@@ -14,10 +14,12 @@ namespace DLLCheck
         static void Main(string[] args)
         {
             Connector connector = new Connector(ConfigurationManager.ConnectionStrings["MoviesPV_522"].ConnectionString);
+            connector.Update("UPDATE Directors SET last_name=N'Tagtgren' WHERE director_id=8");
+            connector.Update("Directors", "first_name", "Michael", "director_id=9");
             connector.Select("*", "Directors");
             connector.Select("title,release_date,first_name,last_name", "Movies,Directors", "director = director_id");
             Connector connectorAcademy = new Connector(ConfigurationManager.ConnectionStrings["PV_522_Import"].ConnectionString);
-            connectorAcademy.Select("*", "Disciplines");
+            //connectorAcademy.Select("*", "Disciplines");
         }
     }
 }
