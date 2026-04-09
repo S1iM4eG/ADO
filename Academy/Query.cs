d3fbf6234b8ec7ef3da93c8906d0f1adeab0f886
S1iM <S1iM@HOME-PC>
2026-04-09 21:37:12 +0400

    Custom queries on tabd

---
+++ Academy/Query.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+     class Query
+    {
+        public string Fields { get; set; }
+        public string Tables { get; set; }
+        public string Conditions { get; set; }
+        public Query(string fields, string tables, string condition = "")
+        { 
+            this.Fields = fields;
+            this.Tables = tables;
+            this.Conditions = condition;
+        }
+        public override string ToString()
+        {
+            string cmd = $"SELECT {Fields} FROM {Tables}";
+            if (Conditions != "") cmd += $" WHERE {Conditions}";
+            return cmd;
+        }
+    }
+}
