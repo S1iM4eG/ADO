38060df8860ace365dd0396b2061b72ccaf31339
S1iM <S1iM@HOME-PC>
2026-04-09 19:19:01 +0400

    1 little change

--- Academy/MainForm.Designer.cs
+++ Academy/MainForm.Designer.cs
@@ -154,6 +154,9 @@
             // 
             // dgvDisciplines
             // 
+            this.dgvDisciplines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvDisciplines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDisciplines.Location = new System.Drawing.Point(6, 35);
             this.dgvDisciplines.Name = "dgvDisciplines";
